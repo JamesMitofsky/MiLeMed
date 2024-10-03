8e2d47b97cdfd01eb7134133be082df888f32e27
James Mitofsky <JamesMitofsky@gmail.com>
2024-10-03 21:20:07 +0200

    Remove Home title from landing screen

--- apps/expo/app/(drawer)/(tabs)/_layout.tsx
+++ apps/expo/app/(drawer)/(tabs)/_layout.tsx
@@ -18,7 +18,7 @@ export default function Layout() {
     <>
       <Stack.Screen
         options={{
-          title: 'Home',
+          title: '',
           headerShown: pathname === '/' || pathname === '/create',
           headerTintColor: accentColor.val,
           headerLeft: () => (
