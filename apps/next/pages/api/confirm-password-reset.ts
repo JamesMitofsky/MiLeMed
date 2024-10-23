221822337a927c2419c103bcdf44f34398d86e2e
James Mitofsky <JamesMitofsky@gmail.com>
2024-10-24 00:17:06 +0200

    Refine to use pages next 14 pattern for api

--- apps/next/pages/api/confirm-password-reset.ts
+++ apps/next/pages/api/confirm-password-reset.ts
@@ -1,36 +1,22 @@
-import { createClient, type EmailOtpType } from '@supabase/supabase-js'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-import { Database } from '../../../../supabase/types'
+import { supabaseAdmin } from '../../../../packages/api/src/supabase-admin'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    // Combine host and req.url to create a full URL
-    const baseUrl = `https://milemed.de`
-    const { searchParams } = new URL(req.url!, baseUrl) // Full URL required here
+  const { token_hash, type, next = '/' } = req.query
+  const redirectTo = next as string
 
-    const token_hash = searchParams.get('token_hash')
-    const type = searchParams.get('type') as EmailOtpType | null
-    const next = searchParams.get('next') ?? '/reset-password' // Default to reset-password page
-
-    if (token_hash && type) {
-      const supabase = createClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.SUPABASE_SERVICE_ROLE!
-      )
-
-      const { error } = await supabase.auth.verifyOtp({ type, token_hash })
-      if (!error) {
-        const redirectTo = new URL(next, baseUrl)
-        return res.redirect(redirectTo.toString()) // Use res.redirect in API routes
-      }
+  if (token_hash && type) {
+    const { error } = await supabaseAdmin.auth.verifyOtp({
+      type: type as EmailOtpType,
+      token_hash: token_hash as string,
+    })
+    if (!error) {
+      return res.redirect(redirectTo)
     }
-
-    // Return to error page on failure
-    const errorRedirect = new URL('/auth/auth-code-error', baseUrl)
-    return res.redirect(errorRedirect.toString())
-  } catch (error) {
-    console.error('Error processing request:', error)
-    return res.status(500).json({ message: 'Internal Server Error' })
   }
+
+  // Redirect to an error page if token verification fails
+  return res.redirect('/auth/auth-code-error')
 }
