a66d6c656b3f240321540ce86a8a209392b4a549
James Mitofsky <JamesMitofsky@gmail.com>
2024-11-01 19:21:40 +0100

    Let users finish their sign up through the app

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,15 +1,24 @@
 import { ScrollView, YStack, XStack } from '@my/ui'
 
+import { CompleteRegistration } from './components/CompleteRegistration'
 import { ChaptersPreviewList } from './components/achievements-section'
 import { StatisticsPreviewList } from './components/overview-section'
+import { useUser } from '../../utils/useUser'
 
 export function HomeScreen() {
+  const { profile } = useUser()
   return (
     <XStack maw={1480} als="center" ai="flex-start" f={1}>
       <ScrollView f={1} fb={0}>
         <YStack gap="$7" pb="$10" pt="$5">
-          <ChaptersPreviewList />
-          <StatisticsPreviewList />
+          {!profile || !profile.role ? (
+            <CompleteRegistration />
+          ) : (
+            <>
+              <ChaptersPreviewList />
+              <StatisticsPreviewList />
+            </>
+          )}
           {/* <FeedbackPreview /> */}
         </YStack>
       </ScrollView>
