39677c30d77a5990360e0ad9203b08cf147f3607
James Mitofsky <JamesMitofsky@gmail.com>
2024-11-01 11:39:18 +0100

    Remove insert and update types, add in missing Enums

--- packages/app/utils/supabase/databaseTypes.ts
+++ packages/app/utils/supabase/databaseTypes.ts
@@ -11,60 +11,26 @@ type QuizAnswersType = Database['public']['Tables']['user_quiz_answers']['Row']
 type QuizQuestionOptionsType = Database['public']['Tables']['quiz_question_options']['Row']
 type QuizQuestionsType = Database['public']['Tables']['quiz_questions']['Row']
 
-// Insert types
-type AppEventsInsertType = Database['public']['Tables']['app_events']['Insert']
-type ChaptersInsertType = Database['public']['Tables']['chapters']['Insert']
-type ImagesInsertType = Database['public']['Tables']['images']['Insert']
-type LectureEventsInsertType = Database['public']['Tables']['lecture_events']['Insert']
-type LecturesInsertType = Database['public']['Tables']['lectures']['Insert']
-type ProfilesInsertType = Database['public']['Tables']['profiles']['Insert']
-type QuizAnswersInsertType = Database['public']['Tables']['user_quiz_answers']['Insert']
-type QuizQuestionOptionsInsertType = Database['public']['Tables']['quiz_question_options']['Insert']
-type QuizQuestionsInsertType = Database['public']['Tables']['quiz_questions']['Insert']
-
-// Update types
-type AppEventsUpdateType = Database['public']['Tables']['app_events']['Update']
-type ChaptersUpdateType = Database['public']['Tables']['chapters']['Update']
-type ImagesUpdateType = Database['public']['Tables']['images']['Update']
-type LectureEventsUpdateType = Database['public']['Tables']['lecture_events']['Update']
-type LecturesUpdateType = Database['public']['Tables']['lectures']['Update']
-type ProfilesUpdateType = Database['public']['Tables']['profiles']['Update']
-type QuizAnswersUpdateType = Database['public']['Tables']['user_quiz_answers']['Update']
-type QuizQuestionOptionsUpdateType = Database['public']['Tables']['quiz_question_options']['Update']
-type QuizQuestionsUpdateType = Database['public']['Tables']['quiz_questions']['Update']
-
 // Enums
 type GenderType = Database['public']['Enums']['gender']
 type ModeType = Database['public']['Enums']['mode']
+type QuestionType = Database['public']['Enums']['question_type']
+type QuizEventType = Database['public']['Enums']['quiz_event_type']
+type UserRoleType = Database['public']['Enums']['user_role']
 
 export {
   AppEventsType,
-  AppEventsInsertType,
-  AppEventsUpdateType,
   ChaptersType,
-  ChaptersInsertType,
-  ChaptersUpdateType,
   ImagesType,
-  ImagesInsertType,
-  ImagesUpdateType,
   LectureEventsType,
-  LectureEventsInsertType,
-  LectureEventsUpdateType,
   LecturesType,
-  LecturesInsertType,
-  LecturesUpdateType,
   ProfilesType,
-  ProfilesInsertType,
-  ProfilesUpdateType,
   QuizAnswersType,
-  QuizAnswersInsertType,
-  QuizAnswersUpdateType,
   QuizQuestionOptionsType,
-  QuizQuestionOptionsInsertType,
-  QuizQuestionOptionsUpdateType,
   QuizQuestionsType,
-  QuizQuestionsInsertType,
-  QuizQuestionsUpdateType,
   GenderType,
   ModeType,
+  QuestionType,
+  QuizEventType,
+  UserRoleType,
 }
