9304949e349b6676356b337827add6bcc2f81dca
James Mitofsky <JamesMitofsky@gmail.com>
2024-11-12 12:20:17 +0100

    Fix copy: improve translation

--- packages/app/features/chapters/ViewChaptersScreen.tsx
+++ packages/app/features/chapters/ViewChaptersScreen.tsx
@@ -17,7 +17,7 @@ const ViewChaptersScreen = () => {
           <Switch checked={mode === 'THEORETICAL'} onCheckedChange={toggleMode} size="$2">
             <Switch.Thumb borderColor="$color1" animation="200ms" />
           </Switch>
-          <SizableText>{mode === 'THEORETICAL' ? 'Vorlesung' : 'Praktisch'}</SizableText>
+          <SizableText>{mode === 'THEORETICAL' ? 'Vorlesung' : 'Blockpraktikum'}</SizableText>
         </XStack>
         <ListOfChapters mode={mode === 'THEORETICAL' ? 'THEORETICAL' : 'PRACTICAL'} />
       </YStack>
