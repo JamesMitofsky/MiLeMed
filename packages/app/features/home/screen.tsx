13ee00a635abf66bc3a403912ec2b8d74a8a587f
James Mitofsky <JamesMitofsky@gmail.com>
2024-11-23 19:31:20 +0100

    Fix: move react-native confetti call into its own trigger function to avoid accidental calls.

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, YStack, XStack, FullscreenSpinner, useToastController } from '@my/ui'
-import { useCallback, useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { Confetti, ConfettiMethods } from 'react-native-fast-confetti'
 
 import { FinishRegistrationForm } from './components/FinishRegistrationForm'
@@ -11,32 +11,36 @@ export function HomeScreen() {
   const { profile } = useUser()
   const confettiRef = useRef<ConfettiMethods>(null)
   const toast = useToastController()
+  const [isMainPageVisible, setIsMainPageVisible] = useState(false)
 
   const handleRegistrationSuccess = useCallback(() => {
     confettiRef.current?.restart()
     toast.show('Profil erfolgreich aktualisiert.', { type: 'success' })
+    setIsMainPageVisible(true)
   }, [confettiRef])
 
   useEffect(() => {
     if (profile?.role) {
-      handleRegistrationSuccess()
+      setIsMainPageVisible(true)
+    } else {
+      setIsMainPageVisible(false)
     }
-  }, [profile?.role, handleRegistrationSuccess])
+  }, [profile?.role, setIsMainPageVisible])
 
   return (
     <XStack als="center" ai="flex-start" f={1}>
       <Confetti autoplay={false} fadeOutOnEnd ref={confettiRef} />
       <ScrollView f={1} fb={0}>
-        <YStack gap="$7" pb="$10" pt="$5">
+        <YStack gap="$7" pb="$10" pt="$5" f={1}>
           {!profile ? (
             <FullscreenSpinner />
-          ) : !profile.role ? (
-            <FinishRegistrationForm />
-          ) : (
+          ) : isMainPageVisible ? (
             <>
               <ChaptersPreviewList />
               <StatisticsPreviewList />
             </>
+          ) : (
+            <FinishRegistrationForm onSuccess={handleRegistrationSuccess} />
           )}
           {/* <FeedbackPreview /> */}
         </YStack>
