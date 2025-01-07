31c0033da56bc064eac0318638a5a9242202ed0a
James Mitofsky <jamesmitofsky@gmail.com>
2025-01-07 10:46:24 +0100

    Upload milemed video

--- apps/next/pages/qvk2025.tsx
+++ apps/next/pages/qvk2025.tsx
@@ -1,24 +1,15 @@
-import Image from 'next/image'
-import { Stack, SizableText, Spinner, XStack } from 'tamagui'
+import { Stack } from 'tamagui'
 
 const VideoProcessingPage = () => {
   return (
     <Stack flex={1} alignItems="center" justifyContent="center" gap="$6">
-      <XStack ai="center" gap="$4">
-        <SizableText size="$9">Videoverarbeitung</SizableText>
-        <Spinner size="large" color="black" />
-      </XStack>
-      <Image
-        src="/logo.png"
-        alt="Logo"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '100%', maxWidth: '10rem', height: 'auto' }}
-      />
-      <SizableText size="$5">
-        Bitte kommen Sie später noch einmal zurück, um das gerenderte Video anzusehen.
-      </SizableText>
+      <video controls style={{ maxWidth: '100%' }} preload="metadata">
+        <source src="/milemed-2025-grant.mp4" type="video/mp4" />
+        <p>
+          Your browser does not support HTML video. Please{' '}
+          <a href="/milemed-2025-grant.mp4">download the video</a> instead.
+        </p>
+      </video>
     </Stack>
   )
 }
