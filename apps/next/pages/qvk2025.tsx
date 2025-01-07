ea64dc3bdc77f1288c30b0e4309cfc1c2c05b337
James Mitofsky <jamesmitofsky@gmail.com>
2025-01-07 18:41:49 +0100

    Update video title to better summarize the video

--- apps/next/pages/qvk2025.tsx
+++ apps/next/pages/qvk2025.tsx
@@ -4,10 +4,10 @@ const VideoProcessingPage = () => {
   return (
     <Stack flex={1} alignItems="center" justifyContent="center" gap="$6">
       <video controls style={{ maxWidth: '100%' }} preload="metadata">
-        <source src="/milemed-2024-recap.mp4" type="video/mp4" />
+        <source src="/2024-recap.mp4" type="video/mp4" />
         <p>
           Your browser does not support HTML video. Please{' '}
-          <a href="/milemed-2024-recap.mp4">download the video</a> instead.
+          <a href="/2024-recap.mp4">download the video</a> instead.
         </p>
       </video>
     </Stack>
