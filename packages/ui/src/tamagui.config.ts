1435cbbc0012f196e183e695ba1262762dce5bdd
James Mitofsky <jamesmitofsky@gmail.com>
2025-06-21 13:03:13 -0400

    Fixup: real fix to the styles

--- packages/ui/src/tamagui.config.ts
+++ packages/ui/src/tamagui.config.ts
@@ -21,10 +21,8 @@ setupDev({
  * This avoids shipping themes as JS. Instead, Tamagui will hydrate them from CSS.
  */
 
-const themes =
-  process.env.TAMAGUI_TARGET !== 'web' || process.env.TAMAGUI_IS_SERVER || process.env.STORYBOOK
-    ? themesIn
-    : ({} as typeof themesIn)
+// Always use themes regardless of environment to prevent 'Missing theme' errors
+const themes = themesIn
 
 export const config = createTamagui({
   ...defaultConfig,
