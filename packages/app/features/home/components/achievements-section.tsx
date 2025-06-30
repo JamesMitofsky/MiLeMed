68f7f9cf1cc0dfa3f8924fe502295a5a19013a10
James Mitofsky <jamesmitofsky@gmail.com>
2025-06-30 17:23:21 -0400

    Improve mode switching in mobile app

--- packages/app/features/home/components/achievements-section.tsx
+++ packages/app/features/home/components/achievements-section.tsx
@@ -1,5 +1,5 @@
-import { Button, H2, SizableText, Switch, XStack, YStack } from '@my/ui'
-import { ArrowRight, LibraryBig } from '@tamagui/lucide-icons'
+import { Button, H2, SizableText, Switch, Theme, XStack, YStack } from '@my/ui'
+import { ArrowRight, BookOpen, Stethoscope } from '@tamagui/lucide-icons'
 import { useRouter } from 'solito/router'
 
 import ListOfChapters from './list-of-chapters'
@@ -18,9 +18,11 @@ export const ChaptersPreviewList = () => {
     <YStack>
       <XStack px="$4.5" ai="center" jc="space-between" mb="$5">
         <XStack ai="center" gap="$2">
-          <H2 theme="alt1" fow="400">
-            <LibraryBig size={25} />
-          </H2>
+          {mode === 'THEORETICAL' ? (
+            <BookOpen size="$2" color="$blue10" />
+          ) : (
+            <Stethoscope size="$2" color="$green10" />
+          )}
           <H2 theme="alt1" fow="400">
             {' '}
             Kapitel
@@ -39,11 +41,21 @@ export const ChaptersPreviewList = () => {
         </Button>
       </XStack>
 
-      <XStack mx="$5" mb="$5" gap="$4" onPress={toggleMode}>
-        <Switch checked={mode === 'THEORETICAL'} onCheckedChange={toggleMode} size="$2">
-          <Switch.Thumb borderColor="$color1" animation="200ms" />
-        </Switch>
-        <SizableText>{mode === 'THEORETICAL' ? 'Vorlesung' : 'Blockpraktikum'}</SizableText>
+      <XStack mx="$5" mb="$5" gap="$4" ai="center" onPress={toggleMode}>
+        <Theme name={mode === 'PRACTICAL' ? 'green' : 'light_blue_active'}>
+          <Switch
+            id="mode-switch"
+            checked={mode === 'PRACTICAL'}
+            onCheckedChange={(checked) => setMode(checked ? 'PRACTICAL' : 'THEORETICAL')}
+            size="$4"
+            theme={mode === 'PRACTICAL' ? 'green' : 'blue'}
+          >
+            <Switch.Thumb animation="quick" />
+          </Switch>
+          <SizableText color={mode === 'THEORETICAL' ? '$blue10' : '$green10'}>
+            {mode === 'THEORETICAL' ? 'Vorlesung' : 'Blockpraktikum'}
+          </SizableText>
+        </Theme>
       </XStack>
       <ScrollAdapt>
         <XStack px="$4" fw="wrap" f={1} gap="$3">
