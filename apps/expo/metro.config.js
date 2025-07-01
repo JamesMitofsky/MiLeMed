dfca58b53119595407fb7b57a737acd6a6a153d7
James Mitofsky <jamesmitofsky@gmail.com>
2025-07-01 15:14:31 -0400

    Switch to local versioning to allow for builds on my machine

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,27 +1,29 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-/**
- * @type {import('expo/metro-config')}
- */
-const { getDefaultConfig } = require('@expo/metro-config')
+// metro.config.js
+// Learn more: https://docs.expo.dev/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config') // ← CHANGE THIS LINE
 const path = require('path')
 
 const projectRoot = __dirname
-const workspaceRoot = path.resolve(__dirname, '../..')
+const workspaceRoot = path.resolve(__dirname, '../..') // monorepo root
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(projectRoot)
 
+// ---- your custom tweaks ----
 config.watchFolders = [workspaceRoot]
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ]
 
-// https://github.com/supabase/supabase-js/issues/1400#issuecomment-2843653869
+// Work-around for supabase-js export bug
 config.resolver.unstable_enablePackageExports = false
 
-config.transformer = { ...config.transformer, unstable_allowRequireContext: true }
-config.transformer.minifierPath = require.resolve('metro-minify-terser')
-
-config.resetCache = true
+// Allow require.context + use terser
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+  minifierPath: require.resolve('metro-minify-terser'),
+}
 
 module.exports = config
