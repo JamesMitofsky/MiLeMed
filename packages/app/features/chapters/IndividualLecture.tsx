8d1f6fabd36b5760b5e70caa00b97582cd303a0f
James Mitofsky <jamesmitofsky@gmail.com>
2025-06-30 23:55:53 -0400

    Simplify single lecture file layout

--- packages/app/features/chapters/IndividualLecture.tsx
+++ packages/app/features/chapters/IndividualLecture.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import Markdown from 'react-native-markdown-display'
 import { useRouter } from 'solito/router'
-import { YStack, Text, ScrollView, SizableText } from 'tamagui'
+import { YStack, Text, SizableText } from 'tamagui'
 
 import { ThemeContext } from '../../provider/theme/UniversalThemeProvider.native'
 import { useLectureById, useLectures } from '../../utils/hooks/queryHooks'
@@ -31,56 +31,55 @@ const IndividualLecture = ({ lectureId }: IndividualLectureProps) => {
   const context = useContext(ThemeContext)
 
   return (
-    <ScrollView>
-      <YStack p="$4" gap="$4">
-        {isLoading ? (
-          <Skeleton height={200} width="100%" />
-        ) : lecture ? (
-          <>
-            <YStack gap="$2">
-              <SizableText size="$8" fontWeight="800">
-                {lecture.title}
-              </SizableText>
-              {/* <Theme name="alt1">
+    <YStack p="$4" gap="$4" bg="white" flex={1}>
+      {isLoading ? (
+        <Skeleton height={200} width="100%" />
+      ) : lecture ? (
+        <>
+          <YStack gap="$2">
+            <SizableText size="$8" fontWeight="800">
+              {lecture.title}
+            </SizableText>
+            {/* <Theme name="alt1">
                 <SizableText size="$3">{lecture.subtitle}</SizableText>
               </Theme> */}
-            </YStack>
-            <Markdown
-              style={{
-                body: {
-                  color: context?.current === 'dark' ? '#fff' : '#000',
-                  fontSize: 16,
-                },
-                heading1: {
-                  color: context?.current === 'dark' ? '#fff' : '#000',
-                  fontSize: 24,
-                  marginTop: 8,
-                  marginBottom: 8,
-                },
-                heading2: {
-                  color: context?.current === 'dark' ? '#fff' : '#000',
-                  fontSize: 20,
-                  marginTop: 8,
-                  marginBottom: 8,
-                },
-                heading3: {
-                  color: context?.current === 'dark' ? '#fff' : '#000',
-                  fontSize: 18,
-                  marginTop: 8,
-                  marginBottom: 8,
-                },
-                image: {
-                  maxHeight: 400,
-                  width: '100%',
-                  height: '100%',
-                  resizeMode: 'contain',
-                },
-              }}
-            >
-              {lecture.content}
-            </Markdown>
-            {/* TODO: Important: check if quiz exists */}
-            {/* {lecture.has_quiz ? (
+          </YStack>
+          <Markdown
+            style={{
+              body: {
+                color: context?.current === 'dark' ? '#fff' : '#000',
+                fontSize: 16,
+              },
+              heading1: {
+                color: context?.current === 'dark' ? '#fff' : '#000',
+                fontSize: 24,
+                marginTop: 8,
+                marginBottom: 8,
+              },
+              heading2: {
+                color: context?.current === 'dark' ? '#fff' : '#000',
+                fontSize: 20,
+                marginTop: 8,
+                marginBottom: 8,
+              },
+              heading3: {
+                color: context?.current === 'dark' ? '#fff' : '#000',
+                fontSize: 18,
+                marginTop: 8,
+                marginBottom: 8,
+              },
+              image: {
+                maxHeight: 400,
+                width: '100%',
+                height: '100%',
+                resizeMode: 'contain',
+              },
+            }}
+          >
+            {lecture.content}
+          </Markdown>
+          {/* TODO: Important: check if quiz exists */}
+          {/* {lecture.has_quiz ? (
               <Button size="$5" onPress={handleNavigateToQuiz}>
                 Zum Quiz
               </Button>
@@ -89,12 +88,11 @@ const IndividualLecture = ({ lectureId }: IndividualLectureProps) => {
                 Als gelesen markieren
               </Button>
             )} */}
-          </>
-        ) : (
-          <Text>Keine Vorlesung gefunden</Text>
-        )}
-      </YStack>
-    </ScrollView>
+        </>
+      ) : (
+        <Text>Keine Vorlesung gefunden</Text>
+      )}
+    </YStack>
   )
 }
 
